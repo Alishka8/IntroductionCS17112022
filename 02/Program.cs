a28b287210b693c0a703e75ce694c5f75899b933
Alishka <alinka123238@gmail.com>
2022-11-28 17:10:56 +0300

    Lesson 3

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,7 +1,7 @@
 ﻿// С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго (блок-схема)
 
-double a=Convert.ToDouble(Console.ReadLine("Введите число a:"));
-double b=Convert.ToDouble(Console.ReadLine("Введите число b"));
+double a=Convert.ToDouble(Console.ReadLine());
+double b=Convert.ToDouble(Console.ReadLine());
 if (a*a==b) 
 {
     System.Console.WriteLine($"{b} является квадратом {a}");
